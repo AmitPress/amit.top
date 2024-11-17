ee24e6ad951a2dee9a7872e5773498dbeec426cf
amitpress <amitkrsurjo@gmail.com>
2024-11-17 20:52:55 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-motion")],
+}
+
